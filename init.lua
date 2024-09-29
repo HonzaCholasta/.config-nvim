e9b230d36f6305eedb919222519b492c3fed0bad
Jan Cholasta <jan@cholasta.net>
2024-09-29 17:18:27 +0200

    update

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- git clone https://github.com/folke/lazy.nvim ~/.local/share/nvim/lazy/lazy.nvim
 local data_path = vim.fn.stdpath("data") .. ""
 local lazy_path = data_path .. "/lazy/lazy.nvim"
 
@@ -6,9 +7,6 @@ vim.opt.number = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
---vim.opt.selection = "exclusive"
---vim.opt.selectmode = "mouse,key"
---vim.opt.keymodel = "startsel,stopsel"
 vim.opt.cursorline = true
 vim.opt.display = "lastline,uhex"
 vim.opt.listchars = "space:·,tab:↦ ,nbsp:␣,eol:↵"
@@ -19,56 +17,17 @@ vim.opt.updatetime = 500
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set("n", "<Leader>c", "<Cmd>tabclose<CR>")
+vim.keymap.set("n", "<Leader>c", "<Cmd>bdelete<CR>")
 vim.keymap.set("n", "<Leader>h", "<Cmd>nohlsearch<CR>")
-
---vim.keymap.set({ "n", "i", "v" }, "<C-n>", "<Cmd>tabnew<CR>")
---vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<Cmd>write<CR>")
---vim.keymap.set({ "n", "i", "v" }, "<C-w>", "<Cmd>tabclose<CR>")
---vim.keymap.set({ "n", "i", "v" }, "<C-k>", "<Cmd>quit<CR>")
---vim.keymap.set({ "n", "i", "v" }, "<C-q>", "<Cmd>quitall<CR>")
---vim.keymap.set({ "n", "i", "v" }, "<C-Tab>", "<Cmd>normal <C-w>w<CR>")
-
---vim.keymap.set({ "n", "i", "v" }, "<C-z>", "<Cmd>undo<CR>")
---vim.keymap.set({ "n", "i", "v" }, "<C-y>", "<Cmd>redo<CR>")
---vim.keymap.set({ "n", "i", "v" }, "<C-S-z>", "<Cmd>redo<CR>")
-
---vim.keymap.set({ "n", "i" }, "<C-c>", "<Nop>")
---vim.keymap.set({ "n", "i" }, "<C-x>", "<Nop>")
---vim.keymap.set({ "n", "i", "v" }, "<C-v>", "<Cmd>normal gP<CR>")
---vim.keymap.set("v", "<C-c>", "y")
---vim.keymap.set("v", "<C-x>", "x")
+vim.keymap.set("n", "ga", vim.lsp.buf.code_action)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gf", vim.lsp.buf.format)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
 
 vim.keymap.set({ "n", "i", "v" }, "<PageUp>", "<Cmd>normal <C-u><C-u><CR>")
 vim.keymap.set({ "n", "i", "v" }, "<PageDown>", "<Cmd>normal <C-d><C-d><CR>")
---vim.keymap.set({ "n", "i", "x" }, "<PageUp>", "<Cmd>normal <C-u><C-u><CR>")
---vim.keymap.set({ "n", "i", "x" }, "<PageDown>", "<Cmd>normal <C-d><C-d><CR>")
---vim.keymap.set("s", "<PageUp>", "<C-\\><C-n>i<PageUp>", { remap = true })
---vim.keymap.set("s", "<PageDown>", "<C-\\><C-n>i<PageDown>", { remap = true })
---vim.keymap.set("s", "<Left>", "<C-\\><C-n>i", { remap = true })
---vim.keymap.set("s", "<Right>", "<C-\\><C-n>i", { remap = true })
---vim.keymap.set("s", "<Up>", "<C-\\><C-n>i<Up>", { remap = true })
---vim.keymap.set("s", "<Down>", "<C-\\><C-n>i<Down>", { remap = true })
---vim.keymap.set("x", "<Left>", "<S-Left>")
---vim.keymap.set("x", "<Right>", "<S-Right>")
---vim.keymap.set("x", "<Up>", "<S-Up>")
---vim.keymap.set("x", "<Down>", "<S-Down>")
-
---vim.keymap.set("s", "<S-PageUp>", "<S-Cmd>normal <C-u><C-u><CR>")
---vim.keymap.set("s", "<S-PageDown>", "<S-Cmd>normal <C-d><C-d><CR>")
---vim.keymap.set({ "n", "i" }, "<S-PageUp>", "<C-\\><C-n>gh<S-PageUp>", { remap = true })
---vim.keymap.set({ "n", "i" }, "<S-PageDown>", "<C-\\><C-n>gh<S-PageDown>", { remap = true })
---vim.keymap.set("x", "<S-PageUp>", "<PageUp>", { remap = true })
---vim.keymap.set("x", "<S-PageDown>", "<PageDown>", { remap = true })
 
 vim.keymap.set("v", "d", "\"_d")
---vim.keymap.set("v", "<Del>", "\"_d")
---vim.keymap.set("v", "<Backspace>", "\"_d")
-
-vim.keymap.set("v", ">", ">gv")
-vim.keymap.set("v", "<", "<gv")
---vim.keymap.set("v", "<Tab>", ">gv")
---vim.keymap.set("v", "<S-Tab>", "<gv")
 
 vim.diagnostic.config({
   virtual_text = false,
@@ -91,56 +50,10 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
   border = "rounded",
 })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    vim.api.nvim_create_autocmd("CursorHold", {
-      buffer = args.buf,
-      callback = function()
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-
-        -- nvim-navic winbar
-        if client and client.server_capabilities.documentSymbolProvider then
-          require("nvim-navic").attach(client, args.buf)
-          vim.opt_local.winbar = [[%!luaeval("require('nvim-navic').get_location()")]]
-        end
-
-        -- highlight symbol under cursor
-        vim.lsp.buf.document_highlight()
-
-        -- show diagnostics float
-        vim.diagnostic.open_float(nil, {
-          scope = "cursor",
-          source = "always",
-          close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-          focusable = false,
-          border = "rounded",
-        })
-      end,
-    })
-
-    vim.api.nvim_create_autocmd("CursorMoved", {
-      buffer = args.buf,
-      callback = function()
-        -- remove symbol under cursor highlight
-        vim.lsp.buf.clear_references()
-      end,
-    })
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "lua",
-  callback = function()
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.tabstop = 2
-  end,
-})
-
 require("lazy").setup({
   spec = {
     {
       "folke/tokyonight.nvim",
-      lazy = false,
       priority = 1000,
       config = function(_, opts)
         require("tokyonight").setup(opts)
@@ -195,37 +108,6 @@ require("lazy").setup({
 
         require("mason-lspconfig").setup({
           automatic_installation = { exclude = exclude },
-        })
-
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-        require("lspconfig").clangd.setup({
-          capabilities = capabilities,
-        })
-        require("lspconfig").lua_ls.setup({
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              runtime = {
-                version = "LuaJIT",
-              },
-              workspace = {
-                checkThirdParty = false,
-                library = {
-                  vim.env.VIMRUNTIME,
-                },
-              },
-            },
-          },
-        })
-        require("lspconfig").pyright.setup({
-          capabilities = capabilities,
-        })
-        require("lspconfig").ruff_lsp.setup({
-          capabilities = capabilities,
-        })
-        require("lspconfig").rust_analyzer.setup({
-          capabilities = capabilities,
         })
       end,
     },
@@ -338,8 +220,13 @@ require("lazy").setup({
       },
       opts = {
         options = {
-          mode = "tabs",
           separator_style = "slant",
+          custom_filter = function(bufnr)
+            if vim.bo[bufnr].filetype == "qf" then
+              return false
+            end
+            return true
+          end,
         },
       },
     },
@@ -351,6 +238,9 @@ require("lazy").setup({
       },
       opts = {
         options = {
+          disabled_filetypes = {
+            statusline = { "neo-tree" },
+          },
           component_separators = { left = "", right = "" },
           section_separators = { left = "", right = "" },
         },
@@ -394,11 +284,14 @@ require("lazy").setup({
     },
     {
       "rcarriga/nvim-notify",
-      config = true,
+      priority = 999,
+      config = function()
+        vim.notify = require("notify")
+      end,
     },
     {
       "nvim-telescope/telescope.nvim",
-      --branch = "0.1.x",
+      branch = "0.1.x",
       dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
@@ -409,22 +302,7 @@ require("lazy").setup({
         { "<Leader>ff", "<Cmd>Telescope find_files<CR>", "n", silent = true },
         { "<Leader>fg", "<Cmd>Telescope live_grep<CR>", "n", silent = true },
       },
-      opts = function()
-        local actions = require("telescope.actions")
-
-        return {
-          defaults = {
-            mappings = {
-              n = {
-                ["<CR>"] = actions.select_tab_drop,
-              },
-              i = {
-                ["<CR>"] = actions.select_tab_drop,
-              },
-            },
-          },
-        }
-      end,
+      config = true,
     },
     {
       "nvim-neo-tree/neo-tree.nvim",
@@ -442,12 +320,98 @@ require("lazy").setup({
         source_selector = {
           winbar = true,
         },
-        window = {
-          mappings = {
-            ["<CR>"] = "open_tab_drop",
-          },
-        },
       },
     },
   },
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "WinEnter", "CmdWinEnter" }, {
+  callback = function()
+    if vim.bo.filetype == "qf" then
+      vim.opt_local.laststatus = 0
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    -- winbar
+    if client and client.server_capabilities.documentSymbolProvider then
+      require("nvim-navic").attach(client, args.buf)
+      vim.opt_local.winbar = [[%!luaeval("require('nvim-navic').get_location()")]]
+      vim.api.nvim_input("<C-e>")
+    end
+
+    vim.api.nvim_create_autocmd("CursorHold", {
+      buffer = args.buf,
+      callback = function()
+        -- highlight symbol under cursor
+        vim.lsp.buf.document_highlight()
+
+        -- show diagnostics float
+        vim.diagnostic.open_float(nil, {
+          scope = "cursor",
+          source = "always",
+          close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+          focusable = false,
+          border = "rounded",
+        })
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      buffer = args.buf,
+      callback = function()
+        -- remove symbol under cursor highlight
+        vim.lsp.buf.clear_references()
+      end,
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    if vim.bo.filetype == "lua" then
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.tabstop = 2
+    end
+
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    if vim.bo.filetype == "c" or vim.bo.filetype == "cpp" then
+      require("lspconfig").clangd.setup({
+        capabilities = capabilities,
+      })
+    elseif vim.bo.filetype == "lua" then
+      require("lspconfig").lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              version = "LuaJIT",
+            },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+              },
+            },
+          },
+        },
+      })
+    elseif vim.bo.filetype == "python" then
+      require("lspconfig").pyright.setup({
+        capabilities = capabilities,
+      })
+      require("lspconfig").ruff_lsp.setup({
+        capabilities = capabilities,
+      })
+    elseif vim.bo.filetype == "rust" then
+      require("lspconfig").rust_analyzer.setup({
+        capabilities = capabilities,
+      })
+    end
+  end,
 })
