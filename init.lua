125c4965814cc58679e4d288d2a2a41344968853
Jan Cholasta <jan@cholasta.net>
2025-02-26 20:54:12 +0100

    update

--- init.lua
+++ init.lua
@@ -250,8 +250,23 @@ require("lazy").setup({
       dependencies = {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
+        "hrsh7th/nvim-cmp",
       },
-      config = true,
+      opts = {
+        presets = {
+          lsp_doc_border = true,
+        },
+        popupmenu = {
+          backend = "cmp",
+        },
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+      },
     },
     {
       "akinsho/bufferline.nvim",
